3cba7432ffe1ec13065ce57ef6f7bced59511023
Anne Sophie <anne-sophie.vandevelde@epfl.ch>
2018-10-23 17:08:40 +0200

    GD, SGD and LS with MSE

---
+++ utilities.py
@@ -0,0 +1,150 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Utility Functions
+
+def calculate_mse(e):
+    """Calculate the mse for vector e."""
+    return 1/2*np.mean(e**2)
+
+
+def calculate_mae(e):
+    """Calculate the mae for vector e."""
+    return np.mean(np.abs(e))
+
+
+def compute_loss(y, tx, w):
+    """Calculate the loss.
+
+    You can calculate the loss using mse or mae.
+    """
+    e = y - tx.dot(w)
+    return calculate_mse(e)
+
+def compute_gradient(y, tx, w):
+    """Compute the gradient."""
+    err = y - tx.dot(w)
+    grad = -tx.T.dot(err) / len(err)
+    return grad, err
+
+def compute_stoch_gradient(y, tx, w):
+    """Compute a stochastic gradient from just few examples n and their corresponding y_n labels."""
+    err = y - tx.dot(w)
+    grad = -tx.T.dot(err) / len(err)
+    return grad, err
+
+def build_poly(x, degree):
+    """polynomial basis functions for input data x, for j=0 up to j=degree."""
+    poly = np.ones((len(x), 1))
+    for deg in range(1, degree+1):
+        poly = np.c_[poly, np.power(x, deg)]
+    return poly
+
+def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
+    """
+    Generate a minibatch iterator for a dataset.
+    Takes as input two iterables (here the output desired values 'y' and the input data 'tx')
+    Outputs an iterator which gives mini-batches of `batch_size` matching elements from `y` and `tx`.
+    Data can be randomly shuffled to avoid ordering in the original data messing with the randomness of the minibatches.
+    Example of use :
+    for minibatch_y, minibatch_tx in batch_iter(y, tx, 32):
+        <DO-SOMETHING>
+    """
+    data_size = len(y)
+
+    if shuffle:
+        shuffle_indices = np.random.permutation(np.arange(data_size))
+        shuffled_y = y[shuffle_indices]
+        shuffled_tx = tx[shuffle_indices]
+    else:
+        shuffled_y = y
+        shuffled_tx = tx
+    for batch_num in range(num_batches):
+        start_index = batch_num * batch_size
+        end_index = min((batch_num + 1) * batch_size, data_size)
+        if start_index != end_index:
+            yield shuffled_y[start_index:end_index], shuffled_tx[start_index:end_index]
+
+######### Split Data #############
+
+def split_data(x, y, ratio, seed=1):
+    """split the dataset based on the split ratio."""
+    # set seed
+    np.random.seed(seed)
+    # generate random indices
+    num_row = len(y)
+    indices = np.random.permutation(num_row)
+    index_split = int(np.floor(ratio * num_row))
+    index_tr = indices[: index_split]
+    index_te = indices[index_split:]
+    # create split
+    x_tr = x[index_tr]
+    x_te = x[index_te]
+    y_tr = y[index_tr]
+    y_te = y[index_te]
+    return x_tr, x_te, y_tr, y_te
+
+def train_test_split_demo(x, y, degree, ratio, seed):
+    """polynomial regression with different split ratios and different degrees."""
+    x_tr, x_te, y_tr, y_te = split_data(x, y, ratio, seed)
+    # form tx
+    tx_tr = build_poly(x_tr, degree)
+    tx_te = build_poly(x_te, degree)
+
+    weight = least_squares(y_tr, tx_tr)
+
+    # calculate RMSE for train and test data.
+    rmse_tr = np.sqrt(2 * compute_mse(y_tr, tx_tr, weight))
+    rmse_te = np.sqrt(2 * compute_mse(y_te, tx_te, weight))
+
+    print("proportion={p}, degree={d}, Training RMSE={tr:.3f}, Testing RMSE={te:.3f}".format(
+          p=ratio, d=degree, tr=rmse_tr, te=rmse_te))
+    
+def definitive_res(res):      
+    #from the regression, need to choose if we assign +1 or -1. We assign -1 if the value is <0 and +1 if it is >=0.
+    i=0
+    while i < len(res):
+        if res[i] < 0:
+            res[i] = -1
+        else:
+            res[i] = 1
+        i += 1
+    return res
+
+
+####### POLY NEEDED ??? ########## 
+
+
+
+#""" ATTENTION PLOTS NON-COMPRIS"""
+
+#from plots import *
+
+#def polynomial_regression():
+#   """Constructing the polynomial basis function expansion of the data,
+#      and then running least squares regression."""
+#    # define parameters
+#    degrees = [1, 3, 7, 12]
+    
+    # define the structure of the figure
+#    num_row = 2
+#    num_col = 2
+#    f, axs = plt.subplots(num_row, num_col)
+
+#    for ind, degree in enumerate(degrees):
+        # form dataset to do polynomial regression.
+"""        tx = build_poly(x, degree)
+
+        # least squares
+        weights = least_squares(y, tx)
+
+        # compute RMSE
+        rmse = np.sqrt(2 * compute_mse(y, tx, weights))
+        print("Processing {i}th experiment, degree={d}, rmse={loss}".format(
+              i=ind + 1, d=degree, loss=rmse))
+        # plot fit
+        plot_fitted_curve(
+            y, x, weights, degree, axs[ind // num_col][ind % num_col])
+    plt.tight_layout()
+    plt.savefig("visualize_polynomial_regression")
+    plt.show() """
