f30c30f73bc8bdce821a020c538206ea8bd416e7
Rayamy <43442536+Rayamy@users.noreply.github.com>
2018-10-25 14:38:16 +0200

    Update implementation.py

--- implementation.py
+++ implementation.py
@@ -16,13 +16,13 @@ from utilities import *
 def least_squares_GD(y, tx, initial_w, max_iters, gamma):
     """Gradient descent algorithm."""
     # Define parameters to store w and loss
-    ws = [initial_w]
-    losses = []
+    #ws = [initial_w]
+    #losses = []
     w = initial_w
     for n_iter in range(max_iters):
         # compute loss, gradient
         grad, err = compute_gradient(y, tx, w)
-        loss = calculate_mse(err)
+        #loss = compute_mse(y, tx, w)
         # gradient w by descent update
         w = w - gamma * grad
         """# store w and loss
@@ -31,7 +31,7 @@ def least_squares_GD(y, tx, initial_w, max_iters, gamma):
         print("Gradient Descent({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
               bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))"""
 
-    return w
+    return w#,loss
 
 
 ### STOCHASTIC GRADIENT DESCENT ###
@@ -39,8 +39,8 @@ def least_squares_GD(y, tx, initial_w, max_iters, gamma):
 def least_squares_SGD(y, tx, initial_w, batch_size, max_iters, gamma):
     """Stochastic gradient descent."""
     # Define parameters to store w and loss
-    ws = [initial_w]
-    losses = []
+    #ws = [initial_w]
+    #losses = []
     w = initial_w
     
     for n_iter in range(max_iters):
@@ -49,14 +49,14 @@ def least_squares_SGD(y, tx, initial_w, batch_size, max_iters, gamma):
             grad, _ = compute_stoch_gradient(y_batch, tx_batch, w)
             # update w through the stochastic gradient update
             w = w - gamma * grad
-            """# calculate loss
-            loss = compute_loss(y, tx, w)
+            # calculate loss
+            #loss = compute_mse(y, tx, w)
             # store w and loss
-            ws.append(w)
-            losses.append(loss)
-
-        print("SGD({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
-              bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))"""
+            #ws.append(w)
+            #losses.append(loss)
+        #loss=compute_mse(y, tx, w)
+       # print("SGD({bi}/{ti}): loss={l}, w0={w0}, w1={w1}".format(
+        #      bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))"""
     return w
 
 
